ce7596d10b8ff75163d1e5a33068774046b93248
Dinesh Saini <sainidinesh2894@gmail.com>
2024-03-03 07:25:06 +0530

    create new book data

--- books.js
+++ books.js
@@ -8,3 +8,36 @@ main().then((result) => {
 async function main() {
     await mongoose.connect('mongodb://127.0.0.1:27017/amazon');
 }
+
+// Schema validation
+
+const bookSchema = mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    author: {
+        type: String,
+    },
+    price: {
+        type: Number,
+    },
+});
+
+// Book model
+const Book = mongoose.model("Book", bookSchema);
+
+// create some example of book
+
+const book1 = new Book({
+    title: "Mathematics XII",
+    author: "RD Sharma",
+    price: 1200
+});
+
+// save book1
+book1.save().then((res) => {
+    console.log(res)
+}).catch((err) => {
+    console.log(err)
+});
