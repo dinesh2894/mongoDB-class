d5586e6db5b67745ccdca9bbb114133636e13b00
Dinesh Saini <sainidinesh2894@gmail.com>
2024-03-03 06:34:43 +0530

    create user and save

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 const mongoose = require('mongoose');
-// mongoose.connect('mongodb://127.0.0.1:27017/test');
-
 
 main().then((result) => {
     console.log("Connection successfully");
@@ -19,4 +17,41 @@ const userSchema = new mongoose.Schema({
 // TODO: User Model
 const User = mongoose.model('User', userSchema);
 
+User.find({}).then(res => {
+    console.log(res)
+});
+
+
+
+User.updateOne({_id: "65e3433268470fed44d25bd1"}, {age: 20}).then((res) => {
+    console.log(res);
+}).catch((err) => {
+    console.log(err);
+});
+
+User.findById("65e3433268470fed44d25bd1").then((res) => {
+    console.log(res);
+})
+
+// Create user
+// user1 = new User({name: "adam", email: "adam@gmail.com", age: 28});
+// user2 = new User({name: "Eve", email: "eve@gmail.com", age: 48});
+
+
+// Save user
+// user1.save().then(r => {
+//     console.log(r);
+// });
+// user2.save().then(r => {
+//     console.log(r);
+// });
+
+// Create many user at time
+// User.insertMany([
+//     {name: "abc", email: "abc@gmail.com", age: 14},
+//     {name: "efg", email: "efg@gmail.com", age: 15},
+//     {name: "xyz", email: "xyz@gmail.com", age: 16},
+// ]).then((data) => {
+//     console.log(data.length);
+// });
 
